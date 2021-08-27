eabbf2758e5602106145f6c0f543e37b74e7d708
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2021-08-27 22:35:24 +0530

    Uploadin all projects on github

---
+++ AI Face Detection/ReadMe.txt
@@ -0,0 +1,5 @@
+opencv used:
+to install use pip install opencv-python
+
+trained data link:
+https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
---
+++ Netflix Clone/netflix-clone/src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "144bb58d79e8412b019aeef7588a9111";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchToprated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentries:`/discover/movie?api_key=${API_KEY}&with_genres=9`,
+}
+
+export default requests;
